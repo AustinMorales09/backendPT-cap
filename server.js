837c01ef6ef951c9d7f543084c22760b7a3f9ff1
Austin <harrenaustint@gmail.com>
2023-06-07 14:50:35 -0400

    pushing backend

--- server.js
+++ server.js
@@ -30,9 +30,6 @@ app.use(express.json());
 
    if (process.env.NODE_ENV === "production"){
       app.use(express.static('/build'))
-      app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/build'))
-      })
     };
 // Start the server
 app.listen(PORT, () => {
