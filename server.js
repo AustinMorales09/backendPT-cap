a1fbcc598631ae79d67486090382eb9e502eaa00
Austin <harrenaustint@gmail.com>
2023-06-07 18:37:02 -0400

    push

--- server.js
+++ server.js
@@ -1,47 +1,40 @@
 const express = require('express');
-const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-// const artsRouter = require('../backend/routes/arts')
-// const familyRouter = require('../backend/routes/family')
-// const foodRouter = require('../backend/routes/food')
-// const historyRouter = require('../backend/routes/history')
-// const outdoorsRouter = require('../backend/routes/outdoors')
-// const sportsRouter = require('../backend/routes/sports')
-   const attractionsRouter = require('./routes/attractions')
+const uri = process.env.ATLAS_URI;
 
-// app.use('/arts', artsRouter)
-// app.use('/family', familyRouter)
-// app.use('/food', foodRouter)
-// app.use('/history', historyRouter)
-// app.use('/outdoors', outdoorsRouter)
-// app.use('/sports', sportsRouter)
+mongoose.connect(uri, {useNewUrlParser: true,})
+const connection = mongoose.connection;
+
+connection.once('open', () =>{
+  console.log("MongoDB database connection established succesfully");
+})
+// Routes
+
+   const attractionsRouter = require('./routes/attractions')
    app.use('/attractions', attractionsRouter) 
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     next();
+//   });
+
+// Connect to MongoDB
+// comment
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// Connect to MongoDB
-const uri = process.env.ATLAS_URI;
- mongoose.connect(uri, {useNewUrlParser: true,})
-const connection = mongoose.connection;
-connection.once('open', () =>{
-    console.log("MongoDB database connection established succesfully");
-})
-// comment
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+}
